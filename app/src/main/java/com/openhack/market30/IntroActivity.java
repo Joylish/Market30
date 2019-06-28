8d87f6cd9751a973f17db56c3bb422bf734b9ea4
LEEYEONJU <yeonzzu45@ajou.ac.kr>
2019-06-28 12:26:30 +0900

    edit "1226"

--- app/src/main/java/com/openhack/market30/IntroActivity.java
+++ app/src/main/java/com/openhack/market30/IntroActivity.java
@@ -3,22 +3,40 @@ package com.openhack.market30;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
-public class IntroActivity extends Activity{
-    protected void onCreate(Bundle savedInstanceState) {
+import java.util.Random;
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_intro);
-        try {
-            Thread.sleep(4000); // 4초 인트로 화면 보여주기
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+public class IntroActivity extends Activity {
+    Handler handler = new Handler();
+    Runnable r = new Runnable() {
+        @Override
+        public void run() {
+// 4초뒤에 다음화면(MainActivity)으로 넘어가기 Handler 사용
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent); // 다음화면으로 넘어가기
+            finish(); // Activity 화면 제거
         }
+    };
 
-        // next Activity 기재.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((new Random()).nextBoolean() ? R.layout.activity_seller_intro : R.layout.activity_user_intro); // xml과 java소스를 연결
+    } // end of onCreate
 
-        startActivity(new Intent(this, LoginActivity.class));
-        finish();
+    @Override
+    protected void onResume() {
+        super.onResume();
+// 다시 화면에 들어어왔을 때 예약 걸어주기
+        handler.postDelayed(r, 4000); // 4초 뒤에 Runnable 객체 수행
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+// 화면을 벗어나면, handler 에 예약해놓은 작업을 취소하자
+        handler.removeCallbacks(r); // 예약 취소
     }
 }
