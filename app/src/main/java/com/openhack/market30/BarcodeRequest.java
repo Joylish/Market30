c77b54e4b932bf629bea00c7b821d527803b5f43
김반석 <rlaqkstjr96@gmail.com>
2019-06-28 11:09:35 +0900

    for barcode scan

--- app/src/main/java/com/openhack/market30/BarcodeRequest.java
+++ app/src/main/java/com/openhack/market30/BarcodeRequest.java
@@ -1,8 +1,16 @@
 package com.openhack.market30;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Cache;
@@ -16,61 +24,81 @@ import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 
 public class BarcodeRequest extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.barcoderequest);
+        Intent intent = new Intent(this.getIntent());
+        String product_name = intent.getStringExtra("product_name");
+        String img_url = intent.getStringExtra("img_url");
+        Uri imgURI = Uri.parse(img_url);
 
-        final TextView textView = (TextView)findViewById(R.id.textView);
+        final TextView tv1 = (TextView)findViewById(R.id.product_name);
+        final TextInputEditText tv2 = (TextInputEditText)findViewById(R.id.tv2);
+        final TextView tv3 = (TextView)findViewById(R.id.tv3);
+        final TextInputEditText tv4 = (TextInputEditText)findViewById(R.id.tv4);
+        final TextInputEditText tv5 = (TextInputEditText)findViewById(R.id.tv5);
 
-        RequestQueue requestQueue;
+        TextView tv_product_name = (TextView)findViewById(R.id.productName);
+        ImageView iv_img_url = (ImageView)findViewById(R.id.imgURI);
 
-// Instantiate the cache
-        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+        tv_product_name.setText(product_name);
+        iv_img_url.setImageURI(imgURI);
+//        iv_img_url.setImageBitmap(getBitmapFromURL(img_url));
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RequestQueue requestQueue;
+                Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+                Network network = new BasicNetwork(new HurlStack());
+                requestQueue = new RequestQueue(cache, network);
+                requestQueue.start();
+                JSONObject requestBody = new JSONObject();
 
-// Set up the network to use HttpURLConnection as the HTTP client.
-        Network network = new BasicNetwork(new HurlStack());
+                try {
+                    requestBody.put("name", tv1.getText());
+                    requestBody.put("seller_id", "market30");
+                    requestBody.put("price", tv4.getText());
+                    requestBody.put("count", tv3.getText());
+                    requestBody.put("comment", tv2.getText());
+//                requestBody.put("image", re);
 
-// Instantiate the RequestQueue with the cache and network.
-        requestQueue = new RequestQueue(cache, network);
+                } catch(Exception e) {
+                    // check e
+                    Log.d("BarcodeRequest", e.getMessage());
+                }
 
-        requestQueue.start();
+                JsonObjectRequest postStringRequest = new JsonObjectRequest(
+                        Request.Method.POST,
+                        "http://ec2-13-209-77-121.ap-northeast-2.compute.amazonaws.com:6001/api/v1/users/register_product",
+                        requestBody,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Intent intent = new Intent(BarcodeRequest.this, BarcodeScan.class);
+                                startActivity(intent);
+                            }
 
-        JSONObject requestBody = new JSONObject();
-        try {
-            requestBody.put("barcode", 40111896);
-        } catch(Exception e) {
-            // check e
-            Log.d("BarcodeRequest", e.getMessage());
-        }
-
-        JsonObjectRequest postStringRequest = new JsonObjectRequest(
-                Request.Method.POST,
-                "http://ec2-13-209-77-121.ap-northeast-2.compute.amazonaws.com:6001/api/v1/users/verify_barcode",
-                requestBody,
-                new Response.Listener<JSONObject>() {
-                @Override
-                    public void onResponse(JSONObject response) {
-                        Log.d("BarcodeRequest", response.toString());
-                        String a =response.optString("data");
-                        textView.append(a);
-                        //List<String> list = new ArrayList<String>();
-                        // JSONArray array = response.getJSONArray("data");
-                       }
-
-                }, new Response.ErrorListener() {
+                        }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Log.d("BarcodeRequest", error.getMessage());
                     }
                 });
 
-        requestQueue.add(postStringRequest);
-    }
+                requestQueue.add(postStringRequest);
+            }
+        };
 
+        Button register_button = (Button)findViewById(R.id.registerBtn);
+        register_button.setOnClickListener(listener);
+
+    }
 }
--- app/src/main/java/com/openhack/market30/BarcodeScan.java
+++ app/src/main/java/com/openhack/market30/BarcodeScan.java
@@ -1,23 +1,41 @@
 package com.openhack.market30;
 
-        import android.app.Activity;
-        import android.content.Intent;
-        import android.support.annotation.Nullable;
-        import android.support.design.widget.FloatingActionButton;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.support.v7.widget.GridLayoutManager;
-        import android.support.v7.widget.RecyclerView;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.Toast;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-        import com.openhack.market30.adapter.ItemInCardAdapter;
-        import com.openhack.market30.model.ItemInCard;
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.openhack.market30.adapter.ItemInCardAdapter;
+import com.openhack.market30.model.ItemInCard;
 
-        import java.util.ArrayList;
-        import com.google.zxing.integration.android.IntentIntegrator;
-        import com.google.zxing.integration.android.IntentResult;
+import java.util.ArrayList;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class BarcodeScan extends AppCompatActivity {
 
@@ -55,7 +73,61 @@ public class BarcodeScan extends AppCompatActivity {
         if(resultCode == Activity.RESULT_OK){
             IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
             String re = scanResult.getContents();
-            Toast.makeText(this, re, Toast.LENGTH_LONG).show();
+//////////////////////////////////////////////////
+            RequestQueue requestQueue;
+
+// Instantiate the cache
+            Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+// Set up the network to use HttpURLConnection as the HTTP client.
+            Network network = new BasicNetwork(new HurlStack());
+
+// Instantiate the RequestQueue with the cache and network.
+            requestQueue = new RequestQueue(cache, network);
+
+            requestQueue.start();
+
+            JSONObject requestBody = new JSONObject();
+            try {
+                requestBody.put("barcode", re);
+            } catch(Exception e) {
+                // check e
+                Log.d("BarcodeRequest", e.getMessage());
+            }
+
+            JsonObjectRequest postStringRequest = new JsonObjectRequest(
+                    Request.Method.POST,
+                    "http://ec2-13-209-77-121.ap-northeast-2.compute.amazonaws.com:6001/api/v1/users/verify_barcode",
+                    requestBody,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                String product_name = response.getString("product_name");
+                                String img_url = response.getString("img_url");
+
+                                Log.d("BarcodeRequest", product_name);
+                                Log.d("BarcodeResponse", img_url);
+                                Intent intent = new Intent(BarcodeScan.this, BarcodeRequest.class);
+                                intent.putExtra("product_name", product_name);
+                                intent.putExtra("img_url", img_url);
+                                startActivity(intent);
+
+
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("BarcodeRequest", error.getMessage());
+                }
+            });
+
+            requestQueue.add(postStringRequest);
         }
     }
 }
